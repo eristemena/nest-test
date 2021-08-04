34650d0249c0e167d42311b7057409f023b0cd18
Eris Ristemena <e.ristemena@gmail.com>
2021-08-04 17:10:34 +0700

    Modify main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 3000;
   await app.listen(
     process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 3000,
   );
+  console.log(`Running on port ${port}`);
 }
 bootstrap();
